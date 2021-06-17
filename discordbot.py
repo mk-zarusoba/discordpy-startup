6dc7e885f2a538ec54b15ed7848190cf53bda8d4
mk-zarusoba <79084267+mk-zarusoba@users.noreply.github.com>
2021-06-17 18:58:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,30 @@
 from discord.ext import commands
 import os
 import traceback
+from datetime import datetime, timedelta
 
-bot = commands.Bot(command_prefix='/')
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_voice_state_update(member, before, after):
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    if member.guild.id == 852145141909159947 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(852153560993234974)
+        if before.channel is None: 
+            embed = discord.Embed(title="通話開始",color=0x42b3f5)
+            embed.add_field(name="`チャンネル`",value = f'<#{after.channel.id}>')
+            embed.add_field(name="`始めた人`",value = f'{member.mention}')
+            embed.add_field(name="`開始時間`",value = f'{now:%Y/%m/%d \n %H:%M}')
+            await alert_channel.send(embed=embed)
+        elif after.channel is None: 
+            embed = discord.Embed(title="通話開始",color=0xf54842)
+            embed.add_field(name="`チャンネル`",value = f'<#{before.channel.id}>')
+            embed.add_field(name="`始めた人`",value = f'{member.mention}')
+            embed.add_field(name="`終了時間`",value = f'{now:%Y/%m/%d \n %H:%M}')
+            await alert_channel.send(embed=embed)
 
 
 bot.run(token)
